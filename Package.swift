4bd38a2140e8db3d70bace2e03eb0e633bbc0a73
Ruslan <ruslan99992222@gmail.com>
2024-04-10 07:33:55 +0300

    add bot

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.7.2"),
         // 🍃 An expressive, performant, and extensible templating language built for Swift.
         .package(url: "https://github.com/vapor/leaf.git", from: "4.2.4"),
+        .package(url: "https://github.com/nerzh/telegram-vapor-bot", .upToNextMajor(from: "2.1.0"))
     ],
     targets: [
         .executableTarget(
@@ -24,6 +25,7 @@ let package = Package(
                 .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "Leaf", package: "leaf"),
                 .product(name: "Vapor", package: "vapor"),
+                .product(name: "TelegramVaporBot", package: "telegram-vapor-bot"),
             ]
         ),
         .testTarget(name: "AppTests", dependencies: [
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,26 +3,15 @@ import Fluent
 import FluentPostgresDriver
 import Leaf
 import Vapor
+import TelegramVaporBot
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
-
-    app.migrations.add(CreateTodo())
-
-    app.views.use(.leaf)
-
-
-    // register routes
-    try routes(app)
+    let tgApi: String = "7185058155:AAGY5MHEN7I99vtlrVb2uEQFF8c5qKsifKI"
+    TGBot.log.logLevel = app.logger.logLevel
+    let bot: TGBot = .init(app: app, botId: tgApi)
+    await TGBOT.setConnection(try await TGLongPollingConnection(bot: bot))
+    await DefaultBotHandlers.addHandlers(app: app, connection: TGBOT.connection)
+    try await TGBOT.connection.start()
 }
